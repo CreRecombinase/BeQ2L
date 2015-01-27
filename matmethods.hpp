3b097ecae086c7bf839e080b3c0f672106b9d890
Nicholas Knoblauch <nknoblauch@MED-10655.local>
2015-01-27 09:50:22 -0600

    Added RMSE code

--- matmethods.hpp
+++ matmethods.hpp
@@ -2,12 +2,11 @@
 #define MATMETHODS_HPP
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
-#include <boost/timer/timer.hpp>
 #include "armadillo"
 
 using namespace arma;
 using namespace std;
-using boost::timer::auto_cpu_timer;
+
 
 
 umat GenBoot (size_t colsize, size_t bootstrapnumber){
@@ -23,41 +22,72 @@ umat GenBoot (size_t colsize, size_t bootstrapnumber){
   return(samplemat);
 }
 
-void doBoot(mat &A, mat &B,cube &C,umat &BootMat, cube &Summaries){
-  auto_cpu_timer timerInd;
-  timerInd.stop();
-  auto_cpu_timer timerCor;
+mat BootstrapSample(mat &A,umat &Bootmat,int i){
+  return(A.rows(Bootmat.row(i)));
+}
 
-  timerCor.stop();
-  auto_cpu_timer timerMedian;
-  timerMedian.stop();
+void CumMeanBoot(mat &A, mat &B,mat &C,umat &BootMat, mat &means, mat &vars,int bsi){
+
+
+  running_stat_vec<vec> stats;
+
+
+  
+  if(A.n_rows!=BootMat.n_cols){
+    cerr<<"Boot(incorrect indices):  A.n_rows= "<<A.n_rows<<" BM.n_cols= "<<BootMat.n_cols<<endl;
+    throw 15;
+  }
+  else{
+
+    //First iteration of bootstrap
+    mat tA = BootstrapSample(A,BootMat,0);
+    mat tB = BootstrapSample(B,BootMat,0);
+    
+    C = cor(tA,tB);
+    stats(vectorise(C));
+    
+    for(int i=1; i<bsi; ++i){
+      //      cout<<"Boot: "<<i<<endl;
+      tA = BootstrapSample(A,BootMat,i);
+      tB = BootstrapSample(B,BootMat,i);
+      C = cor(tA,tB);
+      stats(vectorise(C));
+    }
+  }
+
+  means = stats.mean();
+  means.reshape(C.n_rows,C.n_cols);
+  vars = stats.var();
+  vars.reshape(C.n_rows,C.n_cols);
+ 
+}
+  
+
+void doBoot(mat &A, mat &B,cube &C,umat &BootMat, cube &Summaries){
   
   if(A.n_rows!=BootMat.n_cols){
     for(int i=1; i<C.n_slices; ++i){
-      cout<<"Boot(incorrect indices): "<<i<<endl;
-      C.slice(i) = cor(A.rows(BootMat(i,span(0,A.n_rows))),B.rows(BootMat(i,span(0,B.n_rows))));
+      cerr<<"Boot(incorrect indices): "<<i<<endl;
+      throw 15;
+      //      C.slice(i) = cor(A.rows(BootMat(i,span(0,A.n_rows))),B.rows(BootMat(i,span(0,B.n_rows))));
     }
   }
   else{
-    timerInd.resume();
-    mat tA = A.rows(BootMat.row(0));
-    mat tB = B.rows(BootMat.row(0));
-    timerInd.stop();
-    timerCor.resume();
+
+    //First iteration of bootstrap
+    mat tA = BootstrapSample(A,BootMat,0);
+    mat tB = BootstrapSample(B,BootMat,0);
+
     C.slice(0) = cor(tA,tB);
-    timerCor.stop();
+
     for(int i=1; i<C.n_slices; ++i){
       cout<<"Boot: "<<i<<endl;
-      timerInd.resume();
-      tA = A.rows(BootMat.row(i));
-      tB = B.rows(BootMat.row(i));
-      timerInd.stop();
-      timerCor.resume();
+      tA = BootstrapSample(A,BootMat,i);
+      tB = BootstrapSample(B,BootMat,i);
       C.slice(i) = cor(tA,tB);
-      timerCor.stop();
     }
   }
-  timerMedian.resume();
+
   for(int i=0; i<C.n_slices; i++){
     mat S = mat(C.n_rows,C.n_slices);
     for(int j=0; j< C.n_cols; j++){
@@ -65,21 +95,19 @@ void doBoot(mat &A, mat &B,cube &C,umat &BootMat, cube &Summaries){
       Summaries.slice(i).col(j)=median(S,1);
     }
   }
-  timerMedian.stop();
-  timerInd.report();
-  timerCor.report();
-  timerMedian.report();
-  
 }
 
 uvec trainindex(const size_t totalsize,const int ksize,const int k){
   uvec returnvec(totalsize-ksize);
-  int j=0;
+  int j=-1;
   for(int i=0; i<totalsize; i++){
     if(i <(k*ksize)||i>((k+1)*ksize)-1){
-      returnvec(j)=i;
       j++;
+      returnvec(j)=i;
     }
+  }
+  if(j+1!=totalsize-ksize){
+    cerr<<"j="<<j<<" returnsize= "<<totalsize-ksize-1<<endl;
   }
   return(returnvec);
 }
@@ -87,12 +115,16 @@ uvec trainindex(const size_t totalsize,const int ksize,const int k){
 
 uvec testindex(const size_t totalsize,const  int ksize,const  int k){
   uvec returnvec(ksize);
-  int j=0;
+  int j=-1;
   for( int i=0; i<totalsize; i++){
     if(i>=(k*ksize)&&i<=((k+1)*ksize-1)){
-      returnvec(j)=i;
       j++;
+      returnvec(j)=i;
+
     }
+  }
+  if(j+1!=ksize){
+    cerr<<"j="<<j<<" ksize= "<<ksize<<endl;
   }
   return(returnvec);
 }
@@ -110,7 +142,7 @@ double RMSE(mat &Est, const mat &True){
 void KfoldCV (const mat &A,const mat &B, const int kfold, const int chunknum, const int bsi,const string file){
 
   mat testA,testB,trainA,trainB;
-  int kfoldIterations = (int) ceil((double)A.n_rows/(double)kfold);
+  int kfoldIterations = (int) floor((double)A.n_rows/(double)kfold);
   if(kfoldIterations<1){
     cerr<<"Must have at least one iteration of cross-validation, not "<<kfoldIterations<<endl;
     throw 12;
@@ -137,37 +169,33 @@ void KfoldCV (const mat &A,const mat &B, const int kfold, const int chunknum, co
   }
   cout<<"Initiating variables and starting kfold: 0"<<endl;
   mat Point(A.n_cols,B.n_cols);
-  cube Summaries(A.n_cols,B.n_cols,bsi);
+  mat means(A.n_cols,B.n_cols);
+  mat vars(A.n_cols,B.n_cols);
+  mat C(A.n_cols,B.n_cols);
+  
   umat BootMat = GenBoot(trainA.n_rows,bsi);
-  cube C(A.n_cols,B.n_cols,bsi);
-  doBoot(trainA,trainB,C,BootMat,Summaries);
+  CumMeanBoot(trainA,trainB,C,BootMat,means,vars,bsi);
   Point = cor(trainA,trainB);
   mat TrueCor = cor(testA,testB);
   double pointSumRMSE = RMSE(Point,TrueCor);
-  vector<double> bootSumRMSE(bsi);
-  for(int i=0; i<bootSumRMSE.size(); i++){
-    bootSumRMSE[i]=RMSE(Summaries.slice(i),TrueCor);
-  }
+  double bootSumRMSE = RMSE(means,TrueCor);
+  
   for(int i=1; i<kfoldIterations; i++){
     cout<<"Starting kfold: "<<i<<endl;
-    testi = testindex(A.n_rows,iter_k,0);
-    traini = trainindex(A.n_rows,iter_k,0);
+    testi = testindex(A.n_rows,iter_k,i);
+    traini = trainindex(A.n_rows,iter_k,i);
     testA = A.rows(testi);
     testB = B.rows(testi);
     trainA = A.rows(traini);
     trainB = B.rows(traini);
-    doBoot(trainA,trainB,C,BootMat,Summaries);
     Point = cor(trainA,trainB);
+    CumMeanBoot(trainA,trainB,C,BootMat,means,vars,bsi);
     TrueCor = cor(testA,testB);
     pointSumRMSE += RMSE(Point,TrueCor);
-    for(int j=0; j<bootSumRMSE.size(); j++){
-      bootSumRMSE[i] += RMSE(Summaries.slice(j),TrueCor);
-    }
+    bootSumRMSE += RMSE(means,TrueCor);
   }
   pointSumRMSE = pointSumRMSE/kfoldIterations;
-  for(int j=0; j<bootSumRMSE.size(); j++){ 
-    bootSumRMSE[j] = bootSumRMSE[j]/kfoldIterations;
-  }
+  bootSumRMSE = bootSumRMSE/kfoldIterations;
   
   ofstream outputfile;
   outputfile.open(file,std::ofstream::out|std::ofstream::app);
@@ -175,9 +203,8 @@ void KfoldCV (const mat &A,const mat &B, const int kfold, const int chunknum, co
   if(chunknum==0){
     outputfile<<"Chunk\tSize\tbsi\tPointSumRMSE\tBootSumRMSE"<<endl;
   }
-  for(int j=0; j<bootSumRMSE.size(); j++){
-    outputfile<<chunknum<<"\t"<<Point.n_elem<<"\t"<<j+1<<"\t"<<pointSumRMSE<<"\t"<<bootSumRMSE[i]<<endl;
-  }
+  outputfile<<chunknum<<"\t"<<Point.n_elem<<"\t"<<"\t"<<pointSumRMSE<<"\t"<<bootSumRMSE<<endl;
+  
   outputfile.close();
 }
     
